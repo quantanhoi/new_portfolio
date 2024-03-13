3bfdc87451ed15ebfd75321fa46ec2584536484e
Ubuntu <ubuntu@ip-172-31-35-134.eu-north-1.compute.internal>
2024-03-13 07:28:55 +0000

    update model

--- newPortfolio/models/base_model.py
+++ newPortfolio/models/base_model.py
@@ -6,4 +6,4 @@ class BaseModel(Model):
     """A base model that will use our PostgreSQL database."""
     class Meta:
         database = db
-        schema = 'db2sttrthie'  #set schema to db2sttrthie
+        #schema = 'db2sttrthie'  #set schema to db2sttrthie
--- newPortfolio/routes/projects.py
+++ newPortfolio/routes/projects.py
@@ -3,6 +3,6 @@ from models.project import Project
 projects_blueprint = Blueprint('projects', __name__)
 @projects_blueprint.route('/api/projects', methods=['GET'])
 def get_projects():
-    query = Project.select()
+    query = Project.select().order_by(Project.id.desc())
     projects_list = [{"id": project.id, "name": project.name, "url": project.url, "icon": project.icon} for project in query]
     return jsonify(projects_list)
